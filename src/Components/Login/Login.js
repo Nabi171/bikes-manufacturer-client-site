701f8a4bd60a9899807f5743371429d6a87daf72
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-05-23 01:57:54 +0600

    login form added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -119,6 +119,7 @@ const Login = () => {
                     </form>
                 </div>
             </div>
+
         </div>
     );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
+import auth from '../../firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from '@firebase/auth';
 const Navbar = () => {
+    const [user] = useAuthState(auth);
+    const logout = () => {
+        signOut(auth);
+    };
     const menuItems = <>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/appointment">Appointment</Link></li>
-        <li><Link to="/review">Review</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-
+        <li><Link className='font-black' to="/">Home</Link></li>
+        <li><Link className='font-black' to="/appointment">Appointment</Link></li>
+        <li><Link className='font-black' to="/review">Review</Link></li>
+        <li><Link className='font-black' to="/register">Register</Link></li>
+        <li>{user ? <button className='font-black' onClick={logout} >Sign Out</button> : <Link className='font-black' to="/login">Login</Link>}</li>
     </>
     return (
         <div className="navbar bg-base-100 navbar-bg">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                    <label tabIndex="0" className="btn btn-primary lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
