be1a3c8d412c9c3fa0460af310d4ae6807e7dcdd
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-05-27 12:18:48 +0600

    admin route created in hooks

--- src/Components/Dassboard/Dassboard.js
+++ src/Components/Dassboard/Dassboard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
+import useAdmin from '../hooks/useAdmin';
 
 
 const Dashboard = () => {
     const [user] = useAuthState(auth);
-
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile navbar-bg">
             <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -18,14 +19,21 @@ const Dashboard = () => {
                 <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     <li><Link to="/dashboard">My Reviews</Link></li>
-                    <li><Link to="/dashboard/myOrder">My Orders</Link></li>
+
                     <li><Link to="/dashboard/addreviews">Add Reviews</Link></li>
                     <li><Link to="/dashboard/allreviews">Reviews</Link></li>
-                    <li><Link to="/dashboard/myprofile">My Profile</Link></li>
-                    <li><Link to="/dashboard/updatedProfile">Update Profile</Link></li>
+
+
                     <li><Link to="/dashboard/addproduct">Add a Product</Link></li>
                     <li><Link to="/dashboard/allproduct">All Product</Link></li>
                     <li><Link to="/dashboard/users">Users</Link></li>
+                    {admin &&
+                        <>
+                            <li><Link to="/dashboard/myprofile">My Profile</Link></li>
+                            <li><Link to="/dashboard/myOrder">My Orders</Link></li>
+                            <li><Link to="/dashboard/updatedProfile">Update Profile</Link></li>
+                        </>
+                    }
                 </ul>
             </div>
         </div>
