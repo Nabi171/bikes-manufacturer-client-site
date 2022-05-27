090792c40c384a2d58867d31c551a79fcfaae394
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-05-27 11:57:44 +0600

    extra section updated

--- src/Components/ExtraSectionthree/ExtraSectionthree.js
+++ src/Components/ExtraSectionthree/ExtraSectionthree.js
@@ -3,12 +3,12 @@ import img from '../ExtraSectionthree/three.png'
 const ExtraSectionthree = () => {
     return (
         <div className='container mx-auto'>
-            <div class="hero min-h-screen navbar-bg">
+            <div class="hero min-h-screen navbar-bg rounded-lg">
                 <div class="hero-content flex-col lg:flex-row-reverse">
                     <img src={img} />
                     <div>
                         <h1 class="text-5xl font-bold">Purchase & Service</h1>
-                        <p class="py-6">If you want to order products ,you have to follow some steps,first you have to sign up,if you  are new user,Go to sign up and make register.Then you can choose the product what you need.After payment,Our man pick your order.Thank You</p>
+                        <p class="py-6 font-bold text-primary">If you want to order products ,you have to follow some steps,first you have to sign up,if you  are new user,Go to sign up and make register.Then you can choose the product what you need.After payment,Our man pick your order.Thank You</p>
                         <ul class="steps steps-vertical">
                             <li class="step step-primary">Sign Up</li>
                             <li class="step step-primary">Purchaes Your Order</li>
@@ -17,7 +17,7 @@ const ExtraSectionthree = () => {
                             <li class="step">Then Pick Your Order</li>
                         </ul>
 
-                        <button class="btn btn-primary">Get Started</button>
+
                     </div>
                 </div>
             </div>
